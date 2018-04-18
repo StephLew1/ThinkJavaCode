96693e41f4779beed179e7da3fc7f064d1625746
StephLew1 <shoalcreek18>
2018-04-18 04:37:12 -0800

    added comment

---
+++ ch02/Assigningvalues.java
@@ -0,0 +1,19 @@
+package PACKAGE_NAME;
+
+public class Assigningvalues
+{
+    public static void main(String[] args)
+    {
+        String txt = "Fantastic";
+        String lang = "Java";
+        txt += lang; // Assign concatenated String
+        System.out.println("Add & Assign Strings;" + txt);
+        int sum = 10;
+        int num = 20;
+        sum += num ; // Assign result ( 10 + 20 = 30 )
+        System.out.println( "Add & Assign Integers: " + sum ) ;
+        int factor = 5 ;
+        sum *= factor ; // Assign result ( 30 x 5 = 150 )
+        System.out.println( "Multiplication sum: " + sum ) ;
+    }
+}
---
+++ ch02/Date.java
@@ -0,0 +1,9 @@
+public class Date
+{
+    public static void main(String[] args)
+    { // gnerate some simple output
+        System.out.println("hello ");
+        int num = 13
+
+    }
+}
