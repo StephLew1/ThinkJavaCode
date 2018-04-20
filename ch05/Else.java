446f859c5df3edc58cfa3bc9a896609ed72cd514
StephLew1 <shoalcreek18>
2018-04-20 02:09:16 -0800

    Completed Comparing Values

---
+++ ch05/Else.java
@@ -0,0 +1,15 @@
+public class Else
+{
+    public static void main(String[] args)
+    {
+        int hrs = 21;
+        if (hrs < 21) ;
+
+        else if (hrs < 18) ;
+
+        {
+         System.out.println(" Good morning; " + hrs);
+        }
+
+    }
+}
