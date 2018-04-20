8c8f9f9c3000cfa130dfcd55ed6e96af3da7fd5d
StephLew1 <shoalcreek18>
2018-04-20 02:30:39 -0800

    Completed Comparing Values

---
+++ ch05/LogicMethods.java
@@ -0,0 +1,21 @@
+public class LogicMethods
+{
+    public staic void main ( String[] args )
+
+        int
+
+    {
+        System.out.println("The number is large ");
+        printIsLarge( number 450 )
+    }
+
+    public static void main ( String[] args )
+
+
+    {
+        if (number > 85) ;
+        {
+            System.out.println
+        }
+    }
+}
