6ffcf90c4b4ccb91c559e7abc6d0797bb76a3262
StephLew1 <shoalcreek18>
2018-04-20 01:54:02 -0800

    Completed Comparing Values

---
+++ ch05/If.java
@@ -0,0 +1,23 @@
+public class If
+{
+
+    public static void main(String[] args)
+
+    {
+        if (5 > 1) System.out.println("Five is greater than one.");
+        if (2 < 4)
+
+        {
+            System.out.println("Test succeeded.");
+            System.out.println("Number is 6-9 inclusive, or 12" );
+
+        }
+
+        int num = 8 ;
+
+        {
+            if (((num > 5) && (num < 10)) || (num == 12))
+                System.out.println("Number is 6-9 inclusive, or 12");
+        }
+    }
+}
